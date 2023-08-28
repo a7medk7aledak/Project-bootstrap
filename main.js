02360032d046cf49a7a6c9ead9225b8aeb3e91be
a7medk7aledak <92623979+a7medk7aledak@users.noreply.github.com>
2023-08-28 20:46:15 +0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-let el = document.querySelector(".scroller");
-let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-window.addEventListener("scroll", () => {
-  const scrollTop = document.documentElement.scrollTop;
-  el.style.width = `${(scrollTop / height) * 100}%`;
-});
