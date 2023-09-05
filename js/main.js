51b6f301019ff9e7239d452215bed3dd2f24ecd2
a7medk7aledak <92623979+a7medk7aledak@users.noreply.github.com>
2023-09-05 11:34:53 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,30 @@ window.addEventListener("scroll", () => {
   const scrollTop = document.documentElement.scrollTop;
   el.style.width = `${(scrollTop / height) * 100}%`;
 });
+
+
+
+
+// shalf list
+let lis = document.querySelectorAll(".out-work .switcher li");
+let imgs = document.querySelectorAll(".out-work .row .all");
+lis.forEach((li) => {
+  li.addEventListener("click", function () {
+    lis.forEach((el)=>{
+      el.classList.remove("active");
+      el.classList.remove("rounded-pill");
+      this.classList.add("active");
+      this.classList.add("rounded-pill");
+    })
+  });
+ 
+  li.addEventListener("click", function () {
+    imgs.forEach((img) => {
+      img.style.display = "none";
+    });
+    document.querySelectorAll(this.dataset.cat).forEach((el) => {
+      el.style.display = "block";
+    });
+  })
+});
+
